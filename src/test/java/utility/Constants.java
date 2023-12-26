cca589ae65f32313e5a4a7467312160a6bc6196d
Vinithkumar <vinith9047kumar@gmail.com>
2023-12-26 15:47:09 +0530

    Changer for run jenkins

--- src/test/java/utility/Constants.java
+++ src/test/java/utility/Constants.java
@@ -5,6 +5,9 @@ public class Constants {
 	
 	
 	
+
+
+
 	public static final String browser= "chrome";
 	public static final String headless= "yes";
 	public static final String nopComm= "https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F";
