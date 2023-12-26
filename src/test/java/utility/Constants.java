eafdb196b3c7ba191aa67fa0662639967c863abb
Vinithkumar <vinith9047kumar@gmail.com>
2023-12-26 17:05:47 +0530

    CucumberAutomation

--- src/test/java/utility/Constants.java
+++ src/test/java/utility/Constants.java
@@ -6,9 +6,6 @@ public class Constants {
 
 
 
-
-
-
 	public static final String browser= "chrome";
 	public static final String headless= "yes";
 	public static final String nopComm= "https://admin-demo.nopcommerce.com/login?ReturnUrl=%2Fadmin%2F";
